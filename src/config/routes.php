1c221823c680c95fc8decf6bdb0ef694c0708c97
Milan Filipek <militofilipek@seznam.cz>
2025-06-29 13:45:12 +0200

    set up project task structure & create basic routing for the app with router from Symfony

---
+++ src/config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+
+$routes = new RouteCollection();
+
+return $routes;
---
+++ src/public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+
+$request = Request::createFromGlobals();
+
+$routes = include dirname(__DIR__) . '/config/routes.php';
+
+$context = new RequestContext();
+$context->fromRequest($request);
+
+$matcher = new UrlMatcher($routes, $context);
+
+try {
+    $parameters = $matcher->match($request->getPathInfo());
+    $controller = $parameters['_controller'];
+    unset($parameters['_controller'], $parameters['_route']);
+    $response = call_user_func_array($controller, $parameters);
+} catch (ResourceNotFoundException $e) {
+    $response = new Response('Request resource wasn\'t found. Please check if your specified URL is correct.', 404);
+} catch (Throwable $e) {
+    $response = new Response('An error occurred during runtime: ' . $e->getMessage(), 500);
+}
+
+$response->send();
